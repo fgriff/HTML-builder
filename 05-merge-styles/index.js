eec6a00e332e35df4a81a8a22b645524c57a9af4
fgriff <anglirum@gmail.com>
2025-01-18 21:23:53 +0300

    feat: implement 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fsp = require('fs/promises');
+const fs = require('fs');
+const { stdout } = require('process');
+
+const createBundle = async () => {
+  try {
+    const srcFolderPath = path.join(__dirname, 'styles');
+    const destFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+    const ws = fs.createWriteStream(destFilePath, { flags: 'w' });
+    const filesList = await fsp.readdir(srcFolderPath, { withFileTypes: true });
+
+    filesList.forEach((file) => {
+      if (path.extname(file.name).slice(1) === 'css') {
+        const srcFilePath = path.join(srcFolderPath, file.name);
+        const rs = fs.createReadStream(srcFilePath);
+
+        rs.on('data', (chunk) => {
+          ws.write(`${chunk}\n`);
+        });
+      }
+    });
+  } catch (error) {
+    stdout.write(error.message);
+  }
+};
+
+createBundle();
